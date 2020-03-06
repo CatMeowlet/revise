e7df28904fbb03bf685cc93cc7da5f8134f0e885
james hipe <jameszzz999888@gmail.com>
2020-03-06 16:01:03 +0800

    change line from acceptReq -rev

--- controller/services/tutor/__acceptRequest.php
+++ controller/services/tutor/__acceptRequest.php
@@ -9,22 +9,24 @@ if (isset($_POST['acceptRequest'])) {
 
 
     //update qualified(tutor)
-    $sql = "UPDATE qualified SET p_id = '$parent_id' status = 'PENDING' WHERE  u_id = '$tutor_id' AND q_id = '$q_id' ";
+    $sql = "UPDATE qualified SET p_id = '$parent_id' , status = 'PENDING' WHERE  u_id = '$tutor_id' AND q_id = '$q_id' ";
     $res_u_qualified = mysqli_query($link, $sql);
-
+    console_log($sql);
     if ($res_u_qualified) {
         $sql_u_request = "UPDATE request SET status='APPROVED' WHERE q_id = '$q_id' AND p_id = '$parent_id' ";
         $res_u_request = mysqli_query($link, $sql_u_request);
+   
         if ($res_u_request) {
             header('location: http://localhost/revise/views/tutor/request.php?update=true');
         } else {
 
-            header('location: http://localhost/revise/views/tutor/request.php?update=failed');
+           header('location: http://localhost/revise/views/tutor/request.php?update=failed');
         }
     } else {
-        header('location: http://localhost/revise/views/tutor/request.php?update=failed');
+       header('location: http://localhost/revise/views/tutor/request.php?update=failed');
     }
 }
+
 if (isset($_POST['cancelRequest'])) {
     $tutor_id = $_SESSION['current_auth_id'];
     $parent_id = $_POST['parent_id'];
